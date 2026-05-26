24cff65d489f4682b5b339e25d9da7f4c03ba01c
guruvajiyeru <guruvajiyerupilli7@gmail.com>
2026-05-26 12:35:44 +0530

    add CRYPTO_SALT export to config

--- server/config/config.js
+++ server/config/config.js
@@ -11,6 +11,7 @@ export const PORT = process.env.PORT || 5000;
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const MONGO_URI = process.env.MONGO_URI;
 export const JWT_SECRET = process.env.JWT_SECRET;
+export const CRYPTO_SALT = process.env.CRYPTO_SALT || 10;
 export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 
 // 🔥 ADD THESE (IMPORTANT)
